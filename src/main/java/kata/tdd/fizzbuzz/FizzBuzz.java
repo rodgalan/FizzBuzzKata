e6688f57830a029ae11c17e9e3f6a51d6355dd9d
Anna Rodriguez Gallego <anna.rodriguez@scmspain.com>
2018-10-13 12:07:59 +0200

    should return FizzBuzz when number is divisible by 5 and has a 3 on it

--- src/main/java/kata/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/kata/tdd/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,7 @@ package kata.tdd.fizzbuzz;
 public class FizzBuzz {
 
   public String applyRule(int number) {
-    return (number % 3 == 0 && number % 5 == 0) ? "FizzBuzz"
+    return ((number % 3 == 0 || String.valueOf(number).contains("3")) && number % 5 == 0) ? "FizzBuzz"
         : (number % 3 == 0 || String.valueOf(number).contains("3")) ? "Fizz"
         : (number % 5 == 0 || String.valueOf(number).contains("5")) ? "Buzz"
         : String.valueOf(number);
