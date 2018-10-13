810df20363a8b1631e9205f37d47fa3862c84f17
Anna Rodriguez Gallego <anna.rodriguez@scmspain.com>
2018-10-13 12:16:02 +0200

    Refactor: extract isFizz and isBuzz logic

--- src/main/java/kata/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/kata/tdd/fizzbuzz/FizzBuzz.java
@@ -3,9 +3,17 @@ package kata.tdd.fizzbuzz;
 public class FizzBuzz {
 
   public String applyRule(int number) {
-    return ((number % 3 == 0 || String.valueOf(number).contains("3")) && (number % 5 == 0 || String.valueOf(number).contains("5"))) ? "FizzBuzz"
-        : (number % 3 == 0 || String.valueOf(number).contains("3")) ? "Fizz"
-        : (number % 5 == 0 || String.valueOf(number).contains("5")) ? "Buzz"
+    return (isFizz(number) && isBuzz(number)) ? "FizzBuzz"
+        : (isFizz(number)) ? "Fizz"
+        : (isBuzz(number)) ? "Buzz"
         : String.valueOf(number);
+  }
+
+  private boolean isBuzz(int number) {
+    return number % 5 == 0 || String.valueOf(number).contains("5");
+  }
+
+  private boolean isFizz(int number) {
+    return number % 3 == 0 || String.valueOf(number).contains("3");
   }
 }
