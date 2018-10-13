f56a3afb14d4a8aef6445dab08bd42716db79d6f
Anna Rodriguez Gallego <anna.rodriguez@scmspain.com>
2018-10-13 11:28:34 +0200

    Test should return the number and feature implementation

--- src/main/java/kata/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/kata/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,8 @@
 package kata.tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+  public String applyRule(int i) {
+    return String.valueOf(i);
+  }
 }
--- src/test/java/kata/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/kata/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,15 @@
 package kata.tdd.fizzbuzz;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FizzBuzzTest {
+
+  @Test
+  public void should_return_the_number() {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    String response = fizzBuzz.applyRule(2);
+    assertEquals("2", response);
+  }
 }
